fab549f28e64113ff5c7041b1dfef1a611cd381b
AmdaaRaijen <bintangtriadmaja@gmail.com>
2026-02-05 18:40:22 +0700

    refactor: move show menu to function

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,18 @@
 #include <iomanip>
 #include <limits>
 
+#include "TechMart.cpp"
+
 using namespace std;
 
 int main()
 {
+
   int choice;
 
   do
   {
-    // Tampilan Menu
-    cout << "\n========================================";
-    cout << "\n   TECHMART INVENTORY MANAGEMENT SYSTEM";
-    cout << "\n========================================";
-    cout << "\n1. Add New Product";
-    cout << "\n2. Show All Products";
-    cout << "\n3. Search Product (by Code)";
-    cout << "\n4. Calculate Total Inventory Value (Recursive)";
-    cout << "\n5. Show Low Stock Products (< 5)";
-    cout << "\n6. Sort Products by Stock";
-    cout << "\n7. Edit Product Stock";
-    cout << "\n8. Delete Product";
-    cout << "\n9. Exit";
-    cout << "\n========================================";
-    cout << "\nSelect option (1-9): ";
+    showMenu();
 
     if (!(cin >> choice))
     {
